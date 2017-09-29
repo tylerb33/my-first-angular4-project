52a08be931a82a84b4b3c1db99b18cc7ac790e18
Tyler Barnett <tyler.barnett7190@gmail.com>
2017-09-29 11:40:13 -0500

    finished component practice

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-warning' ,
+	templateUrl: './warning-alert.component.html',
+	styles: [`
+		h1 {
+			background-color: red;
+			padding: 20px;
+		}
+	`]
+})
+
+export class WarningComponent {
+
+}
